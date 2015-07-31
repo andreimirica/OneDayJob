175e394430e9f15379fe99a2c91530197f4a4fd2
Andrei <bleachexile1@gmail.com>
2015-07-31 14:05:23 +0300

    connect front and back

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,37 +1,13 @@
 'use strict';
 
 angular.module('oneDayJobApp')
-  .controller('MainCtrl', function ($scope, $http, socket) {
-    $scope.awesomeJobs = [];
+  .controller('MainCtrl', function ($scope, $http, socket, Auth, taskFactory) {
+    $scope.awesomeThings = [];
 
-    $http.get('/api/jobs').success(function(awesomeJobs) {
-      $scope.awesomeJobs = awesomeJobs;
-      socket.syncUpdates('job', $scope.awesomeJobs);
-    });
-
-    $scope.addJob = function() {
-      if($scope.newJob === '') {
-        return;
-      }
-      $http.post('/api/jobs', { Title: $scope.newJob });
-      $scope.newJob = '';
-    };
-
-    $scope.deleteJob = function(job) {
-      $http.delete('/api/jobs/' + job._id);
-    };
-
-    $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('job');
-    });
-
-
-$scope.awesomeThings = [];
-
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
-    });
+      $http.get('/api/things').success(function(awesomeThings) {
+        $scope.awesomeThings = awesomeThings;
+        socket.syncUpdates('thing', $scope.awesomeThings);
+      });
 
     $scope.addThing = function() {
       if($scope.newThing === '') {
@@ -40,13 +16,15 @@ $scope.awesomeThings = [];
       $http.post('/api/things', { name: $scope.newThing });
       $scope.newThing = '';
     };
-
+    
     $scope.deleteThing = function(thing) {
       $http.delete('/api/things/' + thing._id);
     };
 
     $scope.$on('$destroy', function () {
       socket.unsyncUpdates('thing');
-    });
+    }); 
 
+    $scope.tasks = taskFactory.getTasks();
+    $scope.isLoggedIn = Auth.isLoggedIn;
   });
--- client/app/main/main.controller.spec.js
+++ client/app/main/main.controller.spec.js
@@ -13,7 +13,7 @@ describe('Controller: MainCtrl', function () {
   // Initialize the controller and a mock scope
   beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
     $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET('/api/jobs')
+    $httpBackend.expectGET('/api/things')
       .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
 
     scope = $rootScope.$new();
@@ -22,8 +22,8 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of jobs to the scope', function () {
+  it('should attach a list of things to the scope', function () {
     $httpBackend.flush();
-    expect(scope.awesomeJobs.length).toBe(4);
+    expect(scope.awesomeThings.length).toBe(4);
   });
 });
