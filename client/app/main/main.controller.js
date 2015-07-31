8a377adff5498f190f1b3d08050df616db3d27dd
Octav Plangu <plangu.octav@gmail.com>
2015-07-31 11:51:02 +0300

    Front end, home page

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,13 +1,13 @@
 'use strict';
 
 angular.module('oneDayJobApp')
-  .controller('MainCtrl', function ($scope, $http, socket) {
+  .controller('MainCtrl', function ($scope, $http, socket, Auth, taskFactory) {
     $scope.awesomeThings = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
-    });
+      $http.get('/api/things').success(function(awesomeThings) {
+        $scope.awesomeThings = awesomeThings;
+        socket.syncUpdates('thing', $scope.awesomeThings);
+      });
 
     $scope.addThing = function() {
       if($scope.newThing === '') {
@@ -16,12 +16,15 @@ angular.module('oneDayJobApp')
       $http.post('/api/things', { name: $scope.newThing });
       $scope.newThing = '';
     };
-
+    
     $scope.deleteThing = function(thing) {
       $http.delete('/api/things/' + thing._id);
     };
 
     $scope.$on('$destroy', function () {
       socket.unsyncUpdates('thing');
-    });
+    }); 
+
+    $scope.tasks = taskFactory.getTasks();
+    $scope.isLoggedIn = Auth.isLoggedIn;
   });
