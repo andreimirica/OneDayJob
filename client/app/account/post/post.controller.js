57fd6eb5e07d799eef7b62b9b82150ad1ed4f5fe
Muscalu Gabriel <gmuscalu5@gmail.com>
2015-08-03 14:15:24 +0300

    ready for test

--- client/app/account/post/post.controller.js
+++ client/app/account/post/post.controller.js
@@ -2,18 +2,20 @@
 
 
 angular.module('oneDayJobApp')
-    .controller('PostCtrl', function($scope, $http, $location, Auth) {
+    .controller('PostCtrl', function($scope, $http, $location, Auth, getCat) {
 
         //     $scope.submit_form = function ($scope) {
 
         //     window.alert("sometext");
         // };
         $scope.getCurrentUser = Auth.getCurrentUser;
+        $scope.currentDate = new Date();
 
         $scope.addJob = function() {
             if ($scope.newJob === '') {
                 return;
             }
+
             $http.post('/api/jobs', {
                 title: $scope.newTitle,
                 description: $scope.newDescription,
@@ -28,11 +30,21 @@ angular.module('oneDayJobApp')
 
             // redirect to home
             $location.path('/');
-
         };
 
-        this.userState = 'Arad';
-        this.states = ('Arad Bacău Brăila Brașov Bucureşti Buzau Cluj-Napoca Constanța Craiova Iași Oradea Pitești Ploiești Satu-Mare Sibiu Târgu-Mureș Timișoara').split(' ').map(function(state) {
+
+
+            getCat.getMongoStuff()
+                .then(function(categories) {
+                    $scope.cat = categories;
+                }),
+                function(error) {
+                    console.error(error);
+                }
+
+        this.userState = '';      
+        this.states = ('Arad Bacău Brăila Brașov Bucureşti Buzau Cluj-Napoca Constanța Craiova Iași Oradea Pitești Ploiești Satu-Mare Sibiu Târgu-Mureș Timișoara')
+        .split(' ').map(function(state) {
             return {
                 abbrev: state
             };
