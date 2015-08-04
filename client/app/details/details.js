8017c6945daa14330b58e41cd2c183fd855abf44
cornelM <manolescu_cornel@yahoo.ro>
2015-08-04 16:25:47 +0300

    details page

--- client/app/details/details.js
+++ client/app/details/details.js
@@ -4,25 +4,11 @@ angular.module('oneDayJobApp')
     .config(function($stateProvider) {
         $stateProvider
             .state('details', {
-                url: '/details',
+                url: '/details//:id',
                 templateUrl: 'app/details/details.html',
                 controller: 'DetailsCtrl'
             });
     });
 
 
-//     title: String,
-//     description: String,
-//     owner: String,
-//     location: String,
-//     category: String,
-//     helpers: Number,
-//     paycheck: Number,
-//     jobDate: Date,
-//     accepted: [],
-//     applicants: [],
-//     comments: [],
-//     createdAt: Date
-// });
 
-// module.exports = mongoose.model('Job', JobSchema);
---
+++ client/app/details/job.service.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('oneDayJobApp')
+    .factory('Job', function($resource) {
+        return $resource('/api/jobs/:id/:controller', {
+            id: '@_id'
+        }, {
+            get: {
+                method: 'GET',
+                params: {
+                    id: 'me'
+                }
+            },
+
+        });
+    });
