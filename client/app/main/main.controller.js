039f2cb7b2fc7ded950ea007e2b97edf97457d77
Andrei <bleachexile1@gmail.com>
2015-08-04 16:42:55 +0300

    fix main ctrl

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,5 +1,3 @@
-'use strict';
-
 
 
 angular.module('oneDayJobApp').filter('cut', function () {
@@ -24,44 +22,44 @@ angular.module('oneDayJobApp').filter('cut', function () {
 
 
 
-// angular.module('oneDayJobApp')
+angular.module('oneDayJobApp')
 
-//     .controller('MainCtrl', function($scope, $http, socket, Auth, taskFactory,$mdDialog) {
+    .controller('MainCtrl', function($scope, $http, socket, Auth, taskFactory,$mdDialog) {
 
-//         $scope.awesomeThings = [];
+        $scope.awesomeThings = [];
 
-//         $http.get('/api/things').success(function(awesomeThings) {
-//             $scope.awesomeThings = awesomeThings;
-//             socket.syncUpdates('thing', $scope.awesomeThings);
-//         });
+        // $http.get('/api/things').success(function(awesomeThings) {
+        //     $scope.awesomeThings = awesomeThings;
+        //     socket.syncUpdates('thing', $scope.awesomeThings);
+        // });
 
-//         $scope.addThing = function() {
-//             if ($scope.newThing === '') {
-//                 return;
-//             }
-//             $http.post('/api/things', {
-//                 name: $scope.newThing
-//             });
-//             $scope.newThing = '';
-//         };
+        // $scope.addThing = function() {
+        //     if ($scope.newThing === '') {
+        //         return;
+        //     }
+        //     $http.post('/api/things', {
+        //         name: $scope.newThing
+        //     });
+        //     $scope.newThing = '';
+        // };
 
-//         $scope.deleteThing = function(thing) {
-//             $http.delete('/api/things/' + thing._id);
-//         };
+        // $scope.deleteThing = function(thing) {
+        //     $http.delete('/api/things/' + thing._id);
+        // };
 
-//         $scope.$on('$destroy', function() {
-//             socket.unsyncUpdates('thing');
-//         });
+        // $scope.$on('$destroy', function() {
+        //     socket.unsyncUpdates('thing');
+        // });
 
-//         $scope.tasks = {};
-//         taskFactory.getMongoStuff()
-//             .then(function(jobs) {
-//                 $scope.tasks = jobs;
-//             }),
-//             function(error) {
-//                 console.error(error);
-//             }
-//         $scope.isLoggedIn = Auth.isLoggedIn;
+        $scope.tasks = {};
+        taskFactory.getMongoStuff()
+            .then(function(jobs) {
+                $scope.tasks = jobs;
+            }),
+            function(error) {
+                console.error(error);
+            }
+        $scope.isLoggedIn = Auth.isLoggedIn;
 
 
 
@@ -87,8 +85,8 @@ angular.module('oneDayJobApp').filter('cut', function () {
 
 
   //-------------------------------------------------------------
-//     }
-// );
+    }
+);
 
 
 
