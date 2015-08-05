309b2ac5cb753590890bd4d953398b68a5640d0b
Andrei <bleachexile1@gmail.com>
2015-08-05 11:42:20 +0300

    ceva

---
+++ client/app/cards/cards.factory.js
@@ -0,0 +1,20 @@
+// 'use strict';
+
+// angular.module('oneDayJobApp')
+//     .factory("taskFactory", function($q, $http) {
+
+//         return {
+//             getMongoStuff: function() {
+//                 var deferred = $q.defer(),
+//                     httpPromise = $http.get('/api/jobs');
+
+//                 httpPromise.success(function(jobs) {
+//                         deferred.resolve(jobs);
+//                     })
+//                     .error(function(error) {
+//                         console.error("Error: " + error);
+//                     });
+//                 return deferred.promise
+//             }
+//         };
+//     });
