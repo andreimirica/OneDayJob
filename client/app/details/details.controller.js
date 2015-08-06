e8878985b3189049d1358f77da1a46f93f8f7f7a
Muscalu Gabriel <gmuscalu5@gmail.com>
2015-08-06 13:54:59 +0300

    aa

--- client/app/details/details.controller.js
+++ client/app/details/details.controller.js
@@ -4,8 +4,7 @@ angular.module('oneDayJobApp')
 
 .controller('DetailsCtrl', function($scope, $http, socket, Auth, taskFactory, $mdDialog, $stateParams, Job) {
 
-    $scope.job = Job.query({id: $stateParams.id});
-
+    $scope.job = Job.get({id: $stateParams.id});
 
     $scope.isLoggedIn = Auth.isLoggedIn;
     $scope.getCurrentUser=Auth.getCurrentUser;
