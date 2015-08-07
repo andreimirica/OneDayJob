a572ea2dddc3dedad509461bd264bc3b24fda898
Mihai-Corneliu Pojar <mihaicorneliu@gmail.com>
2015-08-07 15:15:44 +0300

    Various visual fixes

--- client/app/createButton/button.js
+++ client/app/createButton/button.js
@@ -1,6 +1,6 @@
 angular.module('oneDayJobApp').directive('myButton', function () {
     return {
-        template: '<md-button layout-sm="column" layout-align-sm="end end" class="md-fab md-warn md-hue-2 newJob" aria-label="Profile" ng-show="isLoggedIn()" ng-click="showModal($event)">'+
+        template: '<md-button style="right:20px" layout-sm="column" layout-align-sm="end end" class="md-fab md-warn md-hue-2 newJob" aria-label="Profile" ng-show="isLoggedIn()" ng-click="showModal($event)">'+
 
         '<md-tooltip>Add new announcement</md-tooltip>'+
        ' +'+
