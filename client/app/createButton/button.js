2dbb8cdd2617716518cb66a7fc11c1cd88c7a3f6
Andrew <mirica.andre23@yahoo.com>
2016-06-06 23:15:42 +0300

    Fix floating button + smaller navbar

--- client/app/createButton/button.js
+++ client/app/createButton/button.js
@@ -1,8 +1,8 @@
 angular.module('oneDayJobApp').directive('myButton', function () {
     return {
-        template: '<md-button style="right:20px" layout-sm="column" layout-align-sm="end end" class="md-fab md-warn md-hue-2 newJob" aria-label="Profile" ng-show="isLoggedIn()" ng-click="showModal($event)">'+
+        template: '<md-button style="right:20px; bottom: 10px" layout-sm="column" layout-align-sm="end end" class="md-fab md-warn md-hue-2 newJob" aria-label="Profile" ng-show="isLoggedIn()" ng-click="showModal($event)">'+
 
-        '<md-tooltip>Add new announcement</md-tooltip>'+
+        '<md-tooltip>Add new job</md-tooltip>'+
        ' +'+
     '</md-button>'
     };
